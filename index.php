e9c5b2fdab2e10f899fd9638b22c5d87392c56c8
bishwajit1965 <paul.bishwajit09@gmail.com>
2022-01-16 20:55:25 +0600

    All added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style rel="stylesheet" href="style.css"></style>
+</head>
+<body>
+	<div class="container">
+		<h1>Hello world.</h1>
+		<p>I am a Bangladeshi. I love my counntry very much.</p>
+		
+	</div>
+</body>
+</html>
